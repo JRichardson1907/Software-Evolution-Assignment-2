62909c8cec98188f0b2420b3907c79c9c6af79a0
JoseWongg <j.l.wong@edu.salford.ac.uk>
2023-03-03 10:59:48 +0000

    Upload Cobol2XML

---
+++ Cobol2XML/src/cobol/CobolTest.java
@@ -0,0 +1,226 @@
+package cobol;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CobolTest extends Cobol 
+{
+
+	@Test
+	void testHashCode() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetConstantName() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetConstantName() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetConstantValue() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetConstantValue() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetLineNumber() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetLineNumber() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClone() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIfCobolObjEqual() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCommentLine() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetProgram_ID() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSectionName() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDivisionName() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDayDateWritten() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMonthDateWritten() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetYearDateWritten() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCommentLine() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetProgram_ID() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDayDateWritten() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMonthDateWritten() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetYearDateWritten() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSectionName() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDivisionName() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testObject() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetClass() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject1() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testClone1() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString1() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotify() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNotifyAll() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWait() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWaitLong() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWaitLongInt() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFinalize() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
